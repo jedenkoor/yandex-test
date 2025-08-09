dca96800b6592d5e0450ba1d5e2d98f375c9d556
Даниил Кузьмин <d.kuzmin@leadgid.ru>
2025-08-09 16:06:53 +0300

    first commit

---
+++ src/scripts/app.js
@@ -0,0 +1,25 @@
+class Init {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    const _this = this;
+    this.events();
+  }
+
+  events() {
+    const _this = this;
+  }
+
+  actions() {
+    const _this = this;
+    return {
+
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.controller = new Init();
+});
